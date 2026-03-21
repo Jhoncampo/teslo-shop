84c953b981add33f9cc12368b1d1c15f5779adf9
Eduard <eduardcamayo13@gmail.com>
2026-03-20 19:46:51 -0500

    Mejorando la configuración del módulo de aplicación y añadiendo ConfigModule al módulo de archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,7 @@ import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [] // Todo: Pendiente por cofigurar las variables de entorno
-  }), TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
     type: "postgres",
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT!,
